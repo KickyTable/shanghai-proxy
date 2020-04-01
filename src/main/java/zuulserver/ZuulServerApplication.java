3a1b99d9a7cb1916c50520ed880c17e0dd48c32b
gfilangeri <gfilange@calpoly.edu>
2020-04-01 12:26:08 -0700

    fun stuff

--- src/main/java/zuulserver/ZuulServerApplication.java
+++ src/main/java/zuulserver/ZuulServerApplication.java
@@ -7,13 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
-@Configuration
-@ComponentScan
+
 @EnableAutoConfiguration
-@Controller
 @EnableZuulProxy
 public class ZuulServerApplication {
   public static void main(String[] args) {
-    new SpringApplicationBuilder(ZuulServerApplication.class).web(true).run(args);
+    new SpringApplicationBuilder(ZuulServerApplication.class).run(args);
   }
 }
